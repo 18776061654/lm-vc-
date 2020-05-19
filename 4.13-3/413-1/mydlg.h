21b3a944661cb7adf9830bf8f55b5e90ed4330db
18776061654 <2502760876@qq.com>
2020-05-19 15:27:31 +0800

    2020.4.13-实验3
    2020.4.13-实验3

---
+++ 4.13-3/413-1/mydlg.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "afxwin.h"
+
+
+// mydlg dialog
+
+class mydlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(mydlg)
+
+public:
+	mydlg(CWnd* pParent = NULL);   // standard constructor
+	virtual ~mydlg();
+
+// Dialog Data
+	enum { IDD = IDD_DIALOG1 };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	CListBox lbox;
+	CString s;
+	virtual BOOL OnInitDialog();
+	afx_msg void OnLbnSelchangeList2();
+	CListBox listbox;
+	afx_msg void OnLbnSelchangeList1();
+};
